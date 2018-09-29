0102454856b8e60081fdf9eb1a3d7c005edc2377
Aldwin Su <fangyansu@163.com>
2018-09-29 16:04:47 +0800

    Update

--- devutility.test.common.self/devutility.test.common.self.nio/src/main/java/devutility/test/common/self/nio/usb/DeviceUtils.java
+++ devutility.test.common.self/devutility.test.common.self.nio/src/main/java/devutility/test/common/self/nio/usb/DeviceUtils.java
@@ -1,8 +1,13 @@
 package devutility.test.common.self.nio.usb;
 
+import org.usb4java.Context;
+import org.usb4java.Device;
+import org.usb4java.DeviceDescriptor;
 import org.usb4java.DeviceList;
 import org.usb4java.LibUsb;
 import org.usb4java.LibUsbException;
+
+import devutility.internal.lang.ExceptionHelper;
 
 public class DeviceUtils {
 	public static DeviceList list() {
@@ -14,5 +19,41 @@ public class DeviceUtils {
 		}
 
 		return list;
+	}
+
+	public static Device find(Context context, short vendorId, short productId) {
+		int result = LibUsb.init(context);
+
+		if (result < 0) {
+			throw new LibUsbException("Unable to initialize libusb", result);
+		}
+
+		try {
+			DeviceList devices = new DeviceList();
+			result = LibUsb.getDeviceList(context, devices);
+
+			if (result < 0) {
+				throw new LibUsbException("Unable to get device list", result);
+			}
+
+			for (Device device : devices) {
+				DeviceDescriptor descriptor = new DeviceDescriptor();
+				result = LibUsb.getDeviceDescriptor(device, descriptor);
+
+				if (result < 0) {
+					LibUsbException exception = new LibUsbException("Unable to read device descriptor", result);
+					System.out.println(ExceptionHelper.toString(exception));
+					continue;
+				}
+
+				if (descriptor.idProduct() == productId && descriptor.idVendor() == vendorId) {
+
+				}
+			}
+		} finally {
+			LibUsb.exit(context);
+		}
+
+		return null;
 	}
 }
--- devutility.test.common.self/devutility.test.common.self.nio/src/test/java/devutility/test/common/self/nio/usb4java/UsbScannerTest.java
+++ devutility.test.common.self/devutility.test.common.self.nio/src/test/java/devutility/test/common/self/nio/usb4java/UsbScannerTest.java
@@ -2,6 +2,7 @@ package devutility.test.common.self.nio.usb4java;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.nio.IntBuffer;
 
 import org.usb4java.BufferUtils;
 import org.usb4java.Context;
@@ -16,10 +17,11 @@ import devutility.test.common.self.nio.usb.AsyncEventHandlingThread;
 import devutility.test.common.self.nio.usb.InTransferCallback;
 
 public class UsbScannerTest extends BaseTest {
-	private static final short VENDOR_ID = 0x2010;
-	private static final short PRODUCT_ID = 0x7638;
+	private static final short VENDOR_ID = 0x046d;
+	private static final short PRODUCT_ID = (short) 0xc326;
 	private static final byte INTERFACE_NUMBER = 0;
 	private static final byte ENDPOINT_ADDRESS = (byte) 0x83;
+	private static final long TIMEOUT = 5000;
 
 	@Override
 	public void run() {
@@ -37,47 +39,66 @@ public class UsbScannerTest extends BaseTest {
 				System.err.println("Test device not found.");
 			}
 
-			AsyncEventHandlingThread thread = new AsyncEventHandlingThread();
-			thread.setContext(context);
-			thread.start();
-
 			result = LibUsb.claimInterface(handle, INTERFACE_NUMBER);
 
 			if (result != LibUsb.SUCCESS) {
 				throw new LibUsbException("Unable to claim interface", result);
 			}
 
-			InTransferCallback callback = new InTransferCallback();
-			ByteBuffer buffer = BufferUtils.allocateByteBuffer(8).order(ByteOrder.LITTLE_ENDIAN);
-			Transfer transfer = LibUsb.allocTransfer(0);
-			LibUsb.fillInterruptTransfer(transfer, handle, ENDPOINT_ADDRESS, buffer, callback, null, 5000);
-			result = LibUsb.submitTransfer(transfer);
-
-			if (result != LibUsb.SUCCESS) {
-				throw new LibUsbException("Unable to submit transfer", result);
-			}
-
-			result = LibUsb.releaseInterface(handle, INTERFACE_NUMBER);
-
-			if (result != LibUsb.SUCCESS) {
-				throw new LibUsbException("Unable to release interface", result);
-			}
+			read(handle, 8);
 
 			LibUsb.close(handle);
-
-			thread.abort();
-
-			try {
-				thread.join();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-
 			LibUsb.exit(context);
-			System.out.println("Program finished");
+			System.out.println("Program finished!");
 		} finally {
 			LibUsb.exit(context);
 		}
+	}
+
+	public static ByteBuffer asyncRead(Context context, DeviceHandle handle) {
+		AsyncEventHandlingThread thread = new AsyncEventHandlingThread();
+		thread.setContext(context);
+		thread.start();
+
+		InTransferCallback callback = new InTransferCallback();
+		ByteBuffer buffer = BufferUtils.allocateByteBuffer(8).order(ByteOrder.LITTLE_ENDIAN);
+		Transfer transfer = LibUsb.allocTransfer();
+		LibUsb.fillInterruptTransfer(transfer, handle, ENDPOINT_ADDRESS, buffer, callback, null, 5000);
+		int result = LibUsb.submitTransfer(transfer);
+
+		if (result != LibUsb.SUCCESS) {
+			throw new LibUsbException("Unable to submit transfer", result);
+		}
+
+		result = LibUsb.releaseInterface(handle, INTERFACE_NUMBER);
+
+		if (result != LibUsb.SUCCESS) {
+			throw new LibUsbException("Unable to release interface", result);
+		}
+
+		thread.abort();
+
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		return buffer;
+	}
+
+	public static ByteBuffer read(DeviceHandle handle, int size) {
+		ByteBuffer buffer = BufferUtils.allocateByteBuffer(size).order(ByteOrder.LITTLE_ENDIAN);
+		IntBuffer transferred = BufferUtils.allocateIntBuffer();
+		int result = LibUsb.bulkTransfer(handle, (byte) LibUsb.ENDPOINT_IN, buffer, transferred, TIMEOUT);
+
+		if (result != LibUsb.SUCCESS) {
+			throw new LibUsbException("Unable to read data", result);
+		} else {
+			System.out.println(transferred.get() + " bytes read from device");
+		}
+
+		return buffer;
 	}
 
 	public static void main(String[] args) {
