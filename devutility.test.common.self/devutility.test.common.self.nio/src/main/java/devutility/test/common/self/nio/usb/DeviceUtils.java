312045c3266161f950a35a40b5c4f42112511b5c
Aldwin Su <fangyansu@163.com>
2018-09-29 16:30:09 +0800

    Update

--- devutility.test.common.self/devutility.test.common.self.nio/src/main/java/devutility/test/common/self/nio/usb/DeviceUtils.java
+++ devutility.test.common.self/devutility.test.common.self.nio/src/main/java/devutility/test/common/self/nio/usb/DeviceUtils.java
@@ -21,7 +21,7 @@ public class DeviceUtils {
 		return list;
 	}
 
-	public static Device find(Context context, short vendorId, short productId) {
+	public static UsbDevice find(Context context, short vendorId, short productId) {
 		int result = LibUsb.init(context);
 
 		if (result < 0) {
@@ -36,19 +36,26 @@ public class DeviceUtils {
 				throw new LibUsbException("Unable to get device list", result);
 			}
 
-			for (Device device : devices) {
-				DeviceDescriptor descriptor = new DeviceDescriptor();
-				result = LibUsb.getDeviceDescriptor(device, descriptor);
+			try {
+				for (Device device : devices) {
+					DeviceDescriptor descriptor = new DeviceDescriptor();
+					result = LibUsb.getDeviceDescriptor(device, descriptor);
 
-				if (result < 0) {
-					LibUsbException exception = new LibUsbException("Unable to read device descriptor", result);
-					System.out.println(ExceptionHelper.toString(exception));
-					continue;
+					if (result < 0) {
+						LibUsbException exception = new LibUsbException("Unable to read device descriptor", result);
+						System.out.println(ExceptionHelper.toString(exception));
+						continue;
+					}
+
+					if (descriptor.idProduct() == productId && descriptor.idVendor() == vendorId) {
+						UsbDevice usbDevice = new UsbDevice();
+						usbDevice.setDevice(device);
+						usbDevice.setDeviceDescriptor(descriptor);
+						return usbDevice;
+					}
 				}
-
-				if (descriptor.idProduct() == productId && descriptor.idVendor() == vendorId) {
-
-				}
+			} finally {
+				LibUsb.freeDeviceList(devices, true);
 			}
 		} finally {
 			LibUsb.exit(context);
