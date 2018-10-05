72c052c575fa1280d208105bbf31e221b26c753d
Aldwin Su <fangyansu@163.com>
2018-10-05 18:13:55 +0800

    Move some test cases to external.usb4java

--- devutility.test.common.self/devutility.test.common.self.nio/src/test/java/devutility/test/common/self/nio/BaseTest.java
+++ devutility.test.common.self/devutility.test.common.self.nio/src/test/java/devutility/test/common/self/nio/BaseTest.java
@@ -1,14 +1,5 @@
 package devutility.test.common.self.nio;
 
-import org.usb4java.DeviceHandle;
-import org.usb4java.LibUsb;
-
 public abstract class BaseTest extends devutility.internal.test.BaseTest {
-	protected void showStringDescriptor(String name, final DeviceHandle handle, final byte index) {
-		System.out.format("%s: %x, name: %s\n", name, index, LibUsb.getStringDescriptor(handle, index));
-	}
 
-	protected int toHexInt(byte value) {
-		return value & 0xff;
-	}
 }
