d91dd93fd7fae27532d5b072378e8571fd004d3c
Aldwin Su <fangyansu@163.com>
2018-10-05 21:16:45 +0800

    Add logback.xml

--- devutility.test.common.self/devutility.test.common.self.scanner/src/main/java/devutility/test/common/self/scanner/DUTC_Self_ScannerApp.java
+++ devutility.test.common.self/devutility.test.common.self.scanner/src/main/java/devutility/test/common/self/scanner/DUTC_Self_ScannerApp.java
@@ -2,15 +2,23 @@ package devutility.test.common.self.scanner;
 
 import java.nio.charset.Charset;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import devutility.internal.nio.pipe.ReversePipeThread;
 
 public class DUTC_Self_ScannerApp {
+	private static Logger logger = LoggerFactory.getLogger(DUTC_Self_ScannerApp.class);
+
 	public static void main(String[] args) {
+		logger.info("Starting...");
 		ReversePipeThread reversePipeThread = new ReversePipeThread(System.in, null);
 
 		reversePipeThread.setCallback(buffer -> {
 			if (buffer.hasArray()) {
-				System.out.format("Read from device: %s\n", new String(buffer.array(), Charset.forName("UTF-8")));
+				String value = new String(buffer.array(), Charset.forName("UTF-8"));
+				String logMessage = String.format("Read from device: %s", value);
+				logger.info(logMessage);
 			}
 		});
 
